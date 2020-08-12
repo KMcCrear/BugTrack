8a07c1e08997abe77a8897f75d62a639120fad76
KMcCrear <64017644+KMcCrear@users.noreply.github.com>
2020-08-12 21:16:05 +0100

    Update settings.py

--- bugtrack/settings.py
+++ bugtrack/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e9=sfwi307zx=phv4-)uz0-q#i^(a$8n89%tkb&+0!6k023231'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
